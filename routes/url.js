7bac8f7dd3f0e0efca1fe94d27b109b4265c51f8
kodejeet <roshansahx2003@gmail.com>
2026-04-02 22:42:54 +0530

    feat: add bulk delete functionality and individual copy-to-clipboard buttons to URL table

--- routes/url.js
+++ routes/url.js
@@ -3,9 +3,15 @@ const {
   handleGenerateNewShortURL,
   handleGetAnalytics,
 } = require("../controllers/url"); 
+const URL = require("../models/url");
 const router = express.Router();
 
 router.post("/", handleGenerateNewShortURL);
+
+router.post("/clear-all", async (req, res) => {
+  await URL.deleteMany({ createdBy: req.user._id });
+  return res.redirect("/");
+});
 
 router.get("/analytics/:shortId", handleGetAnalytics);
 
