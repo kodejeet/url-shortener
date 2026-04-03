f28d25e4d51f9eded8147a00fdd084bf0a47c4a1
kodejeet <roshansahx2003@gmail.com>
2026-04-03 15:02:03 +0530

    favicon/logo changes

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
 app.use("/url", restrictToLoggedInUsersOnly, urlRoute);
 app.use("/user", userRoute);
